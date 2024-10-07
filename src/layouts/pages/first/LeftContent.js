f9a6b2ac0017bfddd98ca5dff42a86ece1cefcfb
ltthuong <24749158+ltthuong@users.noreply.github.com>
2024-10-07 14:51:48 +0700

    styles

--- src/layouts/pages/first/LeftContent.js
+++ src/layouts/pages/first/LeftContent.js
@@ -1,7 +1,9 @@
 import React from "react";
 
 function LeftContent() {
-  return <div className="bg-white sm:col-span-12 md:col-span-8 p-7 text-4xl font-extrabold tracking-tight leading-none text-gray-900 md:text-5xl lg:text-6xl dark:text-white">Hi</div>;
+  return <div className="bg-white sm:col-span-12 md:col-span-8 p-7 flex items-center justify-center">
+      <span className="text-4xl font-extrabold tracking-tight leading-none text-gray-900 md:text-5xl lg:text-6xl dark:text-white">Hi</span>
+    </div>;
 }
 
 export default LeftContent;
--- src/layouts/pages/first/RightContent.js
+++ src/layouts/pages/first/RightContent.js
@@ -3,7 +3,7 @@ import SocialLink from "../../../components/SocialLink";
 
 function RightContent() {
   return (
-    <div className="bg-cyan-200 sm:col-span-12 md:col-span-4 p-7">
+    <div className="bg-fuchsia-700 sm:col-span-12 md:col-span-4 p-7">
       <div className="absolute bottom-7 right-7">
         <SocialLink />
       </div>
