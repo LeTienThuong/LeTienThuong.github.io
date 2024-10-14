e96add2da44a86f7f9276b6c9a365b94c4ec75ec
ltthuong <24749158+ltthuong@users.noreply.github.com>
2024-10-14 15:40:43 +0700

    content

--- src/layouts/pages/first/LeftContent.js
+++ src/layouts/pages/first/LeftContent.js
@@ -2,7 +2,7 @@ import React from "react";
 
 function LeftContent() {
   return <div className="bg-white sm:col-span-12 md:col-span-8 p-7 flex items-center justify-center">
-      <span className="text-4xl font-extrabold tracking-tight leading-none text-gray-900 md:text-5xl lg:text-6xl dark:text-white">Hi</span>
+      <span className="text-4xl font-extrabold tracking-tight leading-none text-gray-900 md:text-5xl lg:text-6xl text-black">Hiii</span>
     </div>;
 }
 
